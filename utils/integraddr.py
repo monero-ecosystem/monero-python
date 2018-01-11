66826cd0fbd6138d34f794caacf98c57aee2742a
Michał Sałaban <michal@salaban.info>
2018-01-11 03:14:45 +0100

    Add integrated address util

---
+++ utils/integraddr.py
@@ -0,0 +1,14 @@
+import sys
+from monero.address import address
+from monero.numbers import PaymentID
+
+USAGE = "{0} <address> <payment_id>"
+
+try:
+    addr = address(sys.argv[1])
+    pid = PaymentID(sys.argv[2])
+except IndexError:
+    print(USAGE.format(*sys.argv), file=sys.stderr)
+    sys.exit(-1)
+
+print(addr.with_payment_id(pid))
