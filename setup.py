019d0b6427307d75556fee08ca4a991c731fcc70
Maciej Urbanski <rooter@kyberian.net>
2019-10-30 02:44:37 +0100

    python 3.8 support

--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     keywords = 'monero cryptocurrency',
